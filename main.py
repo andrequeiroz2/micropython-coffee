26c86aa46a33fe74e9a44aa4164da96eda40e928
Houssan Ali Hijazi <hussanhijazi@gmail.com>
2019-11-06 19:57:42 -0300

    add temperature and dht11

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import server
 import mqtt_client
 import wifi
-from board import factory_topic_sub
+from board import factory_topic_pub
+from dht11 import get_data_dht11
 
 is_connected = wifi.connect()
 
@@ -12,5 +13,25 @@ if not is_connected:
 else:
     print("connected wifi")
     topics = factory_topic_sub()
-    mqtt_client.init()
-    mqtt_client.subscribe(topics)
+    mqtt_client.init(True)
+
+    while True:
+        try:
+            client.check_msg()
+        except Exception as e:
+            print("type error: " + str(e))
+
+        sensor = get_data_dht11()
+        temp = str(topics[0])
+        humd = str(topics[1])
+
+        client.publish(topic_temp, temp)
+        client.publish(topic_humd, humd)
+
+        time.sleep(20)
+
+        try:
+            client.check_msg()
+
+        except Exception as e:
+            print("type error: " + str(e))
