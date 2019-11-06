cbda2bffabb43c75ccca64451c4107a2bc3c7199
Houssan Ali Hijazi <hussanhijazi@gmail.com>
2019-11-06 19:26:21 -0300

    Remove unused code

--- main.py
+++ main.py
@@ -11,18 +11,6 @@ if not is_connected:
     server.listen()
 else:
     print("connected wifi")
-    # mac = wifi.getMacAddress()
-    # print(mac)
-    # topic = b"" + (mac + "/coffeeiot").encode()
-    # broker_topic = b"" + (mac + "/coffeeiot/broker").encode()
-    # topics = [topic, broker_topic]
-    #topic = b"" + (mac + "/coffeeiot").encode()
-
     topics = factory_topic_sub()
-
-    # print(topics)
-    # for topic in topics:
-    #     print("ESP8266 is Connected to %s and subscribed to %s topic" %
-    #       ("dasdas", topic))
     mqtt_client.init()
     mqtt_client.subscribe(topics)
