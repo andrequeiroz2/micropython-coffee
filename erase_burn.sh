505081e3829eead94dc460b0682914c4c30ac6ce
Houssan Ali Hijazi <hussanhijazi@gmail.com>
2019-11-07 11:50:36 -0300

    Adjust erase and burn script

--- erase_burn.sh
+++ erase_burn.sh
@@ -12,15 +12,13 @@ else
 fi
 
 if [ type=="esp32" ]; then
-	echo "esp32"
+	echo "Device: esp32 and image: $image"
 	esptool.py --chip esp32 --port /dev/ttyUSB0 erase_flash
 	esptool.py --chip esp32 --port /dev/ttyUSB0 --baud 460800 write_flash -z 0x1000 $image
 else
-	if [type=="esp8266"]; then
-		echo "esp8266"
+	if [ type=="esp8266" ]; then
+		echo "Device: esp8266 and image: $image"
+		esptool.py --port /dev/ttyUSB0 erase_flash
+		esptool.py --port /dev/ttyUSB0 --baud 460800 write_flash --flash_size=detect 0 $image
 	fi	
 fi
-
-exit
-#esptool.py --port /dev/ttyUSB0 erase_flash &&
-#esptool.py --port /dev/ttyUSB0 --baud 460800 write_flash --flash_size=detect 0 esp32-idf3-20190529-v1.11.bin
